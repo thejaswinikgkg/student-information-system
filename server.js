8efe50f97d8930bbd5900db30e0a4661bbf3d2a4
thejaswinikgkg <thejaswinikgkg@gmail.com>
2026-09-06 13:57:00 +0530

    Refactor server to use environment variable for port
    Updated server to listen on configurable port and improved logging.

--- server.js
+++ server.js
@@ -79,6 +79,8 @@ app.delete("/students/:id", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("Server is running on port " + PORT);
 });
